a91f0b4e68b6796cdf6c279579a6c94ba42ec49b
Keshav Sharma <keshavsharma5276@gmail.com>
2023-07-03 18:49:39 +0530

    database

--- configs/db.config.js
+++ configs/db.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    DB_NAME : "idea_app_demo",
-    DB_URL : "mongodb+srv://keshavsharma5276:<Welcome123>@cluster0.qbayauo.mongodb.net/"
+    DB_NAME : "ideaapp",
+    DB_URL : "mongodb://localhost//ideaapp",
 }
